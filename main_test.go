49dfefd7b8cf54156818e06795ef0391818b8da9
Elviie Abdulmennanova <el123abd@gmail.com>
2024-01-02 13:57:28 -0500

    test flag -l to count lines

--- main_test.go
+++ main_test.go
@@ -8,8 +8,17 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b)
+	res := count(b, false)
 	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nword4\nword5\n")
+	exp := 3
+	res := count(b, true)
+	if exp != res {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
 }
